d655f789e202a8046cd388b744a5d81975506bae
Frank Stüber <frank@stueber.de>
2021-01-18 16:09:09 +0100

    Large refactoring

--- src/Configuration/EcfExportOptions.cs
+++ src/Configuration/EcfExportOptions.cs
@@ -1,8 +1,8 @@
-﻿#region ENBREA - Copyright (C) 2020 STÜBER SYSTEMS GmbH
+﻿#region ENBREA - Copyright (C) 2021 STÜBER SYSTEMS GmbH
 /*    
  *    ENBREA
  *    
- *    Copyright (C) 2020 STÜBER SYSTEMS GmbH
+ *    Copyright (C) 2021 STÜBER SYSTEMS GmbH
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU Affero General Public License, version 3,
@@ -26,13 +26,9 @@ namespace Ecf.BbsPlanung
     public class EcfExportOptions
     {
         public string DatabaseConnection { get; set; }
-
         public ICollection<EcfExportFile> Files { get; set; } = new List<EcfExportFile>();
-        
-        public string FolderName { get; set; }
-
         public int SchoolNo { get; set; }
-
         public char Separator { get; set; } = ';';
+        public string TargetFolderName { get; set; }
     }
 }
